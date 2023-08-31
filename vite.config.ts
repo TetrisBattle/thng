b73e1ba00b0fac3bc44fc6629ef4961b50ded4b6
TetrisBattle <thien5596@gmail.com>
2023-08-31 13:50:05 +0300

    Put xtools to its own folder

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+
+export default defineConfig({
+	build: {
+		lib: {
+			entry: resolve(__dirname, './src'),
+			name: 'xTools',
+			fileName: 'xtools',
+		},
+		rollupOptions: {
+			external: ['react', 'react-dom'],
+			output: {
+				globals: {
+					react: 'React',
+					'react-dom': 'ReactDOM',
+				},
+			},
+		},
+	},
+	plugins: [
+		dts({
+			insertTypesEntry: true,
+		}),
+	],
+})
