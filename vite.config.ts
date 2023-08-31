60485d346cddb0ae9505dc512f79042a994fdf99
TetrisBattle <thien5596@gmail.com>
2023-08-31 18:54:50 +0300

    Add playground to test files

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,8 @@
-import { resolve } from 'path'
 import { defineConfig } from 'vite'
-import dts from 'vite-plugin-dts'
+import react from '@vitejs/plugin-react-swc'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-	build: {
-		lib: {
-			entry: resolve(__dirname, './src'),
-			name: 'xTools',
-			fileName: 'xtools',
-		},
-		rollupOptions: {
-			external: ['react', 'react-dom'],
-			output: {
-				globals: {
-					react: 'React',
-					'react-dom': 'ReactDOM',
-				},
-			},
-		},
-	},
-	plugins: [
-		dts({
-			insertTypesEntry: true,
-		}),
-	],
+	plugins: [react(), tsconfigPaths()],
 })
