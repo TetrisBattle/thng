2215274bb80b4966e9600f1d96c37a74819f5678
TetrisBattle <thien5596@gmail.com>
2023-09-08 00:41:54 +0300

    Remove baseUrl

--- react/ui/theme/theme.ts
+++ react/ui/theme/theme.ts
@@ -1,6 +1,6 @@
 import { Theme, ThemeOptions, createTheme } from '@mui/material/styles'
 import { defaultTheme } from './defaultTheme'
-import { mergeDeep } from 'utils/objectHandler'
+import { mergeDeep } from '../../utils/objectHandler'
 
 export const theme = (newTheme: ThemeOptions): Theme => {
 	return createTheme(mergeDeep(defaultTheme, newTheme))
