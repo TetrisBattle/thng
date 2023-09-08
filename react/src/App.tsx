96483d15e78037ad2c67a95e81d0c1f6d05529b0
TetrisBattle <thien5596@gmail.com>
2023-09-08 03:21:32 +0300

    Add vite to test files

---
+++ react/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Box, Typography } from '../main'
+
+export const App = () => {
+	return (
+		<Box>
+			<Typography>App</Typography>
+		</Box>
+	)
+}
---
+++ react/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react-swc'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [react(), tsconfigPaths()],
+	define: {
+		'import.meta.vitest': 'undefined',
+	},
+	test: {
+		includeSource: ['src/**/*.{js,ts}'],
+		coverage: {
+			provider: 'v8',
+			reporter: ['text'],
+		},
+	},
+})
