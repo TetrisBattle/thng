212ea2f07141af0ceae53eaa3250adcea8e2e87c
TetrisBattle <thien5596@gmail.com>
2023-10-06 04:58:52 +0300

    Add Prettify nestedType

--- react/package/utils/typeHandler.ts
+++ react/package/utils/typeHandler.ts
@@ -3,3 +3,8 @@
 // Pick<T, K> = Pick key from type
 // Partial<T> = Make all keys in type optional
 export type PartialKey<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>
+
+// Make nested type more readable
+export type Prettify<T> = {
+	[K in keyof T]: T[K]
+} & object
