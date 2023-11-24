d2613a9ea7cea8c93419de221364c5726dc207a8
TetrisBattle <thien5596@gmail.com>
2023-11-24 22:49:24 +0200

    Update vite to v5

--- dev/vite.config.ts
+++ dev/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from './main'
 import react from '@vitejs/plugin-react-swc'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), tsconfigPaths()],
 })
