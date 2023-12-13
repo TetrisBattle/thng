244c971fb584ea2762ab76b484ffb859b61d1c87
TetrisBattle <thien5596@gmail.com>
2023-12-13 22:24:10 +0200

    Remove eslint file from eslint-plugin-thng Add eslint-plugin-thng to @thng/react

--- react/.eslintrc.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	extends: ['plugin:react/recommended'],
-	rules: {
-		'@typescript-eslint/no-explicit-any': 'warn',
-		'react/react-in-jsx-scope': 'error',
-		'react/display-name': 'off',
-	},
-}
