ed6c80a60a57ced3df1c4ee7a40cbd24db888684
TetrisBattle <thien5596@gmail.com>
2023-12-17 11:50:18 +0200

    Turn off duplicate no-unused-vars warning in typescript

--- eslint-plugin-thng/rules/typescript.js
+++ eslint-plugin-thng/rules/typescript.js
@@ -2,8 +2,8 @@ exports.typescript = {
 	extends: ['plugin:@typescript-eslint/recommended'],
 	parser: '@typescript-eslint/parser',
 	rules: {
-		'@typescript-eslint/no-explicit-any': 'off',
-		'@typescript-eslint/no-unused-vars': 'warn',
 		'no-undef': 'off',
+		'@typescript-eslint/no-unused-vars': 'off',
+		'@typescript-eslint/no-explicit-any': 'off',
 	},
 }
