5c287895080076734589f509f04f62245e796327
TetrisBattle <thien5596@gmail.com>
2023-12-17 12:43:04 +0200

    Remove deepMerge and use extends instead

--- eslint-plugin-thng/main.js
+++ eslint-plugin-thng/main.js
@@ -1,6 +1,5 @@
 'use strict'
 
-const { deepMerge } = require('./utils.js')
 const { base } = require('./rules/base.js')
 const { typescript } = require('./rules/typescript.js')
 const { react } = require('./rules/react.js')
@@ -8,13 +7,28 @@ const { prettier } = require('./rules/prettier.js')
 
 module.exports = {
 	configs: {
-		recommended: deepMerge({}, base, prettier),
-		recommendedTs: deepMerge({}, base, typescript, prettier),
-		recommendedReact: deepMerge({}, base, typescript, react, prettier),
-
 		base: base,
 		typescript: typescript,
 		react: react,
 		prettier: prettier,
+
+		recommended: {
+			extends: ['plugin:thng/base', 'plugin:thng/prettier'],
+		},
+		recommendedTs: {
+			extends: [
+				'plugin:thng/base',
+				'plugin:thng/typescript',
+				'plugin:thng/prettier',
+			],
+		},
+		recommendedReact: {
+			extends: [
+				'plugin:thng/base',
+				'plugin:thng/typescript',
+				'plugin:thng/react',
+				'plugin:thng/prettier',
+			],
+		},
 	},
 }
