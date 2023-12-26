a481251fe22eae998396863500ee1ed3ac4b00ef
TetrisBattle <thien5596@gmail.com>
2023-12-26 19:36:40 +0200

    Eslint: warn about unused vars

--- eslint-plugin-thng/rules/typescript.js
+++ eslint-plugin-thng/rules/typescript.js
@@ -2,8 +2,7 @@ exports.typescript = {
 	extends: ['plugin:@typescript-eslint/recommended'],
 	parser: '@typescript-eslint/parser',
 	rules: {
-		'no-undef': 'off',
-		'@typescript-eslint/no-unused-vars': 'off',
+		'@typescript-eslint/no-unused-vars': 'warn',
 		'@typescript-eslint/no-explicit-any': 'off',
 	},
 }
