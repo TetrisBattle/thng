1f83e65a94760d456183a51a983c23f3c7c80b1d
TetrisBattle <thien5596@gmail.com>
2024-01-07 21:13:41 +0200

    Remove Select component because mui has fixed popper closing problem

--- react/ui/index.ts
+++ react/ui/index.ts
@@ -1,7 +1,3 @@
 export * from '@mui/material'
 export * from './components'
 export * from './theme'
-
-// Overwrite
-export { Select } from './components'
-export type { SelectProps } from './components'
