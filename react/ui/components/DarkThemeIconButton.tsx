1b90be8150f61df633ba6e505af92c6d30976292
TetrisBattle <thien5596@gmail.com>
2024-01-25 21:10:09 +0200

    Add DarkThemeIconButton

---
+++ react/ui/components/DarkThemeIconButton.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Brightness4Outlined as Brightness4OutlinedIcon } from '@mui/icons-material'
+import { IconButton, IconButtonProps } from '@mui/material'
+import { useThemeStore } from '../theme/useThemeStore'
+
+export const DarkThemeIconButton = (iconButtonProps: IconButtonProps) => {
+	const themeStore = useThemeStore()
+
+	return (
+		<IconButton
+			onClick={themeStore.toggleDarkMode}
+			{...iconButtonProps}
+			sx={{ color: 'inherit', ...iconButtonProps.sx }}
+		>
+			<Brightness4OutlinedIcon />
+		</IconButton>
+	)
+}
--- react/ui/index.ts
+++ react/ui/index.ts
@@ -1,5 +1,6 @@
 export * from '@mui/material'
 
+export { DarkThemeIconButton } from './components/DarkThemeIconButton'
 export { LoadingButton } from './components/LoadingButton'
 
 export { BaseThemeProvider } from './theme/BaseThemeProvider'
